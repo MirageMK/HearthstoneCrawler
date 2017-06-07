b16a727a1991fa658c54e2dac340a239fb0f720d
MirageMK <sandro_popovski@yahoo.com>
2017-06-07 16:40:30 +0200

    Get Deck codes in the api

--- HSWebApi/Controllers/DecksController.cs
+++ HSWebApi/Controllers/DecksController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using HSCore;
 using HSCore.Model;
@@ -8,9 +10,9 @@ namespace HSWebApi.Controllers
     public class DecksController : ApiController
     {
         // GET api/cards
-        public IEnumerable<Deck> Get()
+        public IEnumerable<String> Get()
         {
-            return NetDecks.Decks;
+            return NetDecks.Decks.Where(x => x.MyDust == 0).GroupBy(x => x.Class).Select(x => x.OrderBy(y => y.Tier).ThenByDescending(y => y.Dust).First().DeckCode).ToList();
         }
 
         // GET api/values/5
